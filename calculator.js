94b75907373006d570ef6be15c1afffd3443070a
tgrabar <ted.grabar@gmail.com>
2023-10-14 02:39:17 -0700

    Rewrite using object/methods

--- calculator.js
+++ calculator.js
@@ -1,7 +1,93 @@
-const numbers = [];
-let numberEntered = '';
-let mathFunc = '';
-let answerReceived = false;
+function Calculator(firstOperand, secondOperand, operator) {
+  this.firstOperand = firstOperand;
+  this.secondOperand = secondOperand;
+  this.operator = operator;
+  this.answer = '';
+  this.inputs = ['', '', ''];
+  this.calculate = function() {
+    if (!this.answer && this.firstOperand && this.secondOperand) {
+      if (this.operator === '+') this.answer = Number(this.firstOperand) + Number(this.secondOperand);
+      if (this.operator === '-') this.answer = Number(this.firstOperand) - Number(this.secondOperand);
+      if (this.operator === '*') this.answer = Number(this.firstOperand) * Number(this.secondOperand);
+      if (this.operator === '/') this.answer = Number(this.firstOperand) / (Number(this.secondOperand) === 0 ? 1 : Number(this.secondOperand));      
+      updateDisplay();
+      this.firstOperand = '';
+      this.secondOperand = '';
+      this.operator = '';
+      this.inputs.splice(0, 3, '', '', '');
+    }
+  }
+  this.setOperand = function (key) {
+    if (this.answer) this.clear();
+    if (!this.operator) {
+      if (key === '.' && this.firstOperand.includes('.')) return;
+      this.firstOperand += key;
+      this.inputs[0] = this.firstOperand;
+      updateDisplay();
+    } 
+    else {
+      if (key === '.' && this.secondOperand.includes('.')) return;
+      this.secondOperand += key;
+      this.inputs[2] = this.secondOperand;
+      updateDisplay();
+    }
+  }
+  this.setOperator = function (key) {
+    if (this.operator) return;
+    if (this.answer) {
+      this.firstOperand = this.answer;
+      this.operator = key;
+      this.answer = '';
+      this.inputs.splice(0, 3, this.firstOperand, this.operator, '');
+      updateDisplay();
+    }
+    else if (this.firstOperand !== '' && this.secondOperand === '') {
+      this.operator = key;
+      this.inputs[1] = this.operator;
+      updateDisplay();
+    }      
+  }
+  this.clear = function () {
+    this.firstOperand = '';
+    this.secondOperand = '';
+    this.operator = '';
+    this.answer = '';
+    this.inputs.splice(0, 3, '', '', '');
+    updateDisplay();
+  }
+  this.backSpace = function() {
+    if (this.secondOperand) {
+      this.secondOperand = this.secondOperand.slice(0, -1);
+      this.inputs[2] = this.secondOperand;
+      updateDisplay();
+    }
+    else if (this.operator) {
+      this.operator = '';
+      this.inputs[1] = '';
+      updateDisplay();
+    }
+    else if (this.firstOperand) {
+      this.firstOperand = this.firstOperand.slice(0, -1);
+      this.inputs[0] = this.firstOperand;
+      updateDisplay();
+    }
+  }
+  this.negate = function() {
+    if (this.secondOperand) {
+      this.secondOperand = String(Number(this.secondOperand) * -1);
+      this.inputs[2] = this.secondOperand;
+      updateDisplay();
+    }    
+    else if (this.firstOperand && !this.operator) {
+      this.firstOperand = String(Number(this.firstOperand) * -1);
+      this.inputs[0] = this.firstOperand;
+      updateDisplay();
+    }
+  }
+}
+
+const calculator = new Calculator('', '', '');
+
 const numKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const mathKeys = ['+', '-', '*', '/'];
 
@@ -11,155 +97,44 @@ const numBtns = document.querySelectorAll('.num-btn');
 const mathBtns = document.querySelectorAll('.math-btn');
 const equalBtn = document.querySelector('.eq-btn');
 
-document.querySelector('#clear').addEventListener('click', () => clear());
-document.querySelector('#negate').addEventListener('click', () => negate(numberEntered));
-document.querySelector('#backspace').addEventListener('click', () => backSpace());
+document.querySelector('#clear').addEventListener('click', () => calculator.clear());
+document.querySelector('#negate').addEventListener('click', () => calculator.negate());
+document.querySelector('#backspace').addEventListener('click', () => calculator.backSpace());
 
-numBtns.forEach((btn) => { btn.addEventListener('click', e =>  numberEntry(e.target.dataset.key)); });
+numBtns.forEach((btn) => { btn.addEventListener('click', e =>  calculator.setOperand(e.target.dataset.key)); });
 numKeys.forEach(key => document.addEventListener('keydown', (e) => {
   if(e.key === key) {
     keyPressEffect(e);
-    numberEntry(key);
+    calculator.setOperand(key);
     }
   }));
 
-mathBtns.forEach((btn) => { btn.addEventListener('click', e => operatorEntry(e.target.dataset.key)); });
+mathBtns.forEach((btn) => { btn.addEventListener('click', e => calculator.setOperator(e.target.dataset.key)); });
 mathKeys.forEach(key => document.addEventListener('keydown', (e) => {
-  if(e.key === key && !document.querySelector(`button[data-key="${e.key}"]`).hasAttribute('disabled')) {
+  if(e.key === key) {
     keyPressEffect(e);
-    operatorEntry(key);
+    calculator.setOperator(key);
   }
  }));
 
-equalBtn.addEventListener('click', () => equalsEntry());
+equalBtn.addEventListener('click', () => calculator.calculate());
 document.addEventListener('keydown', (e) => {
-  if(e.key === 'Enter' && !equalBtn.hasAttribute('disabled')) {
+  if(e.key === 'Enter') {
     keyPressEffect(e);
-    equalsEntry();
+    calculator.calculate();
   }
 })
 
 document.addEventListener('keydown', (e) => {
   if(e.key === 'Backspace') {
     keyPressEffect(e);
-    backSpace();
+    calculator.backSpace();
   }
 })
 
-function numberEntry(key) {
-  // entering a number after a calculation begins a new one
-  if (answerReceived) clear();
-  // prevent more than one decimal per number
-  if(numberEntered.includes('.') && key === '.') return;
-  numberEntered += key;
-  updateDisplayRow1(key);
-  equalBtn.removeAttribute('disabled');
-}
-
-function operatorEntry(key) {
-  if (numbers.length === 0 && numberEntered !== '') {
-    setFirstNum(numberEntered, key);
-    updateDisplayRow1(key, ' ');
-    disableButtons(mathBtns);
-    answerReceived = false;
-  }
-  else if (numbers.length === 1 && mathFunc === '') {
-    setSecondNum(numberEntered);
-    updateDisplayRow1(key, ' ');
-    disableButtons(mathBtns);
-    equalBtn.removeAttribute('disabled');
-    answerReceived = false;
-  }
-}
-
-function equalsEntry() {
-  if (numbers.length === 1 && mathFunc !== '') {
-    setSecondNum(numberEntered);
-    displayRow1.textContent += ' =';
-    operate(Number(numbers[0]), Number(numbers[1]), mathFunc);
-    enableButtons(mathBtns);
-    equalBtn.setAttribute('disabled', '');
-  }
-}
-
-function operate(num1, num2, func) {
-  let answer = '';
-  switch (func) {
-    case '+': answer = num1 + num2;
-      break;
-    case '-': answer = num1 - num2;
-      break;
-    case '*': answer = num1 * num2;
-      break;
-    case '/': answer = num1 / (num2 === 0 ? 1 : num2);
-      break;
-    default: return 0;
-  }
-  displayAnswer(answer);
-  answerReceived = true;
-}
-
-function setFirstNum(num, operator) {
-  numbers.push(num);
-  numberEntered = '';
-  mathFunc = operator;
-}
-
-function setSecondNum(num) {
-  numbers.push(num);
-  numberEntered = '';
-}
-
-function updateDisplayRow1 (key, addSpace = '') {
-  if(displayRow1.textContent.length <= 35)
-  displayRow1.textContent += addSpace + key + addSpace;
-}
-
-function displayAnswer(answer) {
-  if(displayRow1.textContent.length > 30)
-    displayRow1.textContent = Number.isInteger(answer) ? answer : answer.toFixed(2);
-  else
-    displayRow1.textContent += ' ' + (Number.isInteger(answer) ? answer : answer.toFixed(2));
-  displayRow2.textContent = answer;
-  numbers.length = 0;  
-  numberEntered = answer;
-  mathFunc = '';
-}
-
-function clear() {
-  displayRow1.textContent = '';
-  displayRow2.textContent = '';
-  numbers.length = 0;
-  numberEntered = '';
-  mathFunc = '';
-  answerReceived = false;
-  enableButtons(numBtns);
-  enableButtons(mathBtns);
-  equalBtn.removeAttribute('disabled');
-}
-
-function negate(num) {
-  if(num === '' || num === '0') return;
-  numberEntered = Number(num) * -1;
-  const arr = displayRow1.textContent.split(' ');
-  arr[arr.length - 1] = numberEntered;
-  displayRow1.textContent = arr.join(' ');
-}
-
-function backSpace() {
-  if(numberEntered.length > 0)
-  numberEntered = numberEntered.slice(0, -1);
-  const arr = displayRow1.textContent.split(' ');
-  arr[arr.length - 1] = numberEntered;
-  displayRow1.textContent = arr.join(' ');
-}
-
-function disableButtons (btns) {
-  btns.forEach(btn => btn.setAttribute('disabled', ''));
-}
-
-function enableButtons (btns) {
-  btns.forEach(btn => btn.removeAttribute('disabled'));
+function updateDisplay() {
+  displayRow1.textContent = calculator.inputs.join(' ').replace('*', '×').replace('/', '÷');
+  displayRow2.textContent = calculator.answer;
 }
 
 function keyPressEffect(e) {
